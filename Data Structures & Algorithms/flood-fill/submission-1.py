a81abfc1364caeb38ddf97d9ea3c27e5250bf5a8
Victor Bustillos <68461123+cheveuxdelin@users.noreply.github.com>
2026-07-20 11:26:12 -0700

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        n = len(image)
+        m = len(image[0])
+        initial_color = image[sr][sc]
+        directions = [
+            [0,1],
+            [0,-1],
+            [1,0],
+            [-1,0]
+        ]
+        
+        if initial_color == color:
+            return image
+
+        def in_bounds(i: int, j: int) -> bool:
+            return 0 <= i < n and 0 <= j < m
+
+        stack = [(sr, sc)]
+
+        while stack:
+            i, j = stack.pop()
+
+            image[i][j] = color
+
+            for di, dj in directions:
+                ii = di+i
+                jj = dj+j
+                if in_bounds(ii, jj) and image[ii][jj] == initial_color:
+                    stack.append((ii, jj))
+        return image

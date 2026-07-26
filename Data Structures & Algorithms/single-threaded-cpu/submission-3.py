db6196ffa76459340ce655b029918f904a4722bf
Victor Bustillos <68461123+cheveuxdelin@users.noreply.github.com>
2026-07-26 14:13:13 -0700

    Add: single-threaded-cpu - submission-3

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        result = []
+
+        # (index, (available_at, duration))
+        tasks_ordered_by_arrival = [*enumerate(tasks)]
+        tasks_ordered_by_arrival.sort(key=lambda x: x[1][0])
+        tasks_backlog = collections.deque(tasks_ordered_by_arrival)
+
+        # (duration, index)
+        heap = []
+        t = 0
+
+        while tasks_backlog or heap:
+            while tasks_backlog and tasks_backlog[0][1][0] <= t:
+                index, task = tasks_backlog.popleft()
+                heapq.heappush(heap, (task[1], index))
+            
+            if heap:
+                completion_time, index = heapq.heappop(heap)
+                result.append(index)
+                t += completion_time
+            else:
+                t = tasks_backlog[0][1][0]
+        return result

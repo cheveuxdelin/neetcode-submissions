ab77fa60514778f9194d8ee41eb1ab28e3eb918e
Victor Bustillos <68461123+cheveuxdelin@users.noreply.github.com>
2026-07-28 11:33:55 -0700

    Add: all-paths-from-source-lead-to-destination - submission-1

---
+++ Data Structures & Algorithms/all-paths-from-source-lead-to-destination/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = collections.defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v)
+
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+        state = [0] * n
+
+        def dfs(node) -> bool:
+            if state[node] == VISITING:
+                return False
+            if state[node] == VISITED:
+                return True
+            
+            if len(graph[node]) == 0:
+                return node == destination
+            
+            state[node] = VISITING
+
+            for neighbor in graph[node]:
+                if not dfs(neighbor):
+                    return False
+            
+            state[node] = VISITED
+            return True
+        
+        return dfs(source)
+
+
+
+

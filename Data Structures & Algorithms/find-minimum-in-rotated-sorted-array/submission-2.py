0f49f73d489b71f51dac962e9a0d75d2328c9979
Victor Bustillos <68461123+cheveuxdelin@users.noreply.github.com>
2026-07-28 21:42:47 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,24 @@
+# we're looking for the minimum index where
+# nums[mid] < nums[(mid+1) % n]
+
+# i think we will also again be needing to find the sorted part
+# or not?
+# since we use modulus maybe we avoid that
+
+# new approach, the minimum has to be in the unordered section!!!
+# we keep looking for the unordered section
+# and we will end up finding a number that is something
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] <= nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[left]
